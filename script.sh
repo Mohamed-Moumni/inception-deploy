9bf9592e68f6481fa58fa7ed01b01ee277a7c34b
Mohamed-Moumni <mmoumni@student.1337.ma>
2025-11-22 18:36:55 +0100

    chore: finished deployment to AWS

---
+++ script.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Installation of Aws CLI
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "/tmp/awscliv2.zip"
+
+unzip /tmp/awscliv2.zip
+
+./aws/install
+
+# install Ansible
+
+sudo apt install software-properties-common -y
+
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+
+sudo apt install ansible -y
+
+# install ansible aws community collectoins
+
+ansible-galaxy collection install amazon.aws
+
+# run the playbook
+
+ansible-playbook -i inventory.ini main.yml
